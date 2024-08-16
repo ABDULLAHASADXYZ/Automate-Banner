683d4f521b21a78f1cedc528a88fcbf8a2f49fc3
Abdullah Asad <152769086+ABDULLAHASADXYZ@users.noreply.github.com>
2024-08-16 15:51:14 +0000

    Add files via upload

---
+++ Parrot-install.sh
@@ -0,0 +1,23 @@
+Loop=$HOME/Automate-Banner/
+IL=abdullah
+OP=$HOME/Automate-Banner/abdullah/abdullah
+LY=/usr/local/bin/
+HU=https://github.com/ABDULLAHASADXYZ/abdullah.git
+
+git clone $HU > /dev/null
+
+chmod +x $OP > /dev/null
+
+sudo mv $OP $LY >  /dev/null
+
+LS=/home/$USER/Automate-Banner/abdullah
+
+KP=/home/$USER/.bashrc
+
+if [ ! -f "$KP" ]; then
+    touch "$KP"
+fi
+
+echo "$LS" >> "$KP"
+
+rm -rf $Loop
